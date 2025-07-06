a9622a0457267c175420e38bc5e7261306608d07
Eddu <55552484+St4rman@users.noreply.github.com>
2025-07-06 18:21:35 +0100

    before moving to bp

--- Borne/Intermediate/Build/Win64/UnrealEditor/Inc/Borne/UHT/Borne.init.gen.cpp
+++ Borne/Intermediate/Build/Win64/UnrealEditor/Inc/Borne/UHT/Borne.init.gen.cpp
@@ -17,13 +17,13 @@ void EmptyLinkFunctionForGeneratedCodeBorne_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x9DB34C81,
-				0x5D7226FC,
+				0x6322F62B,
+				0x061E2991,
 				METADATA_PARAMS(0, nullptr)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_Borne.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_Borne.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Borne(Z_Construct_UPackage__Script_Borne, TEXT("/Script/Borne"), Z_Registration_Info_UPackage__Script_Borne, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x9DB34C81, 0x5D7226FC));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Borne(Z_Construct_UPackage__Script_Borne, TEXT("/Script/Borne"), Z_Registration_Info_UPackage__Script_Borne, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x6322F62B, 0x061E2991));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
